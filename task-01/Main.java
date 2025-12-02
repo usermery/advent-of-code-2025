a4e0f1cf032d37396d044126892e3b9b089bf6d8
Meryem Kilic <meryem.kilic@cancom.de>
2025-12-02 06:53:24 +0000

    feat: day 1 task 1

---
+++ task-01/Main.java
@@ -0,0 +1,44 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main{
+    
+    public static void main(String[] args){
+        System.out.println("first result: " + firstTask());
+    }
+
+    private static List<String> getInput(String file){
+        List<String> input = new ArrayList<>();
+        try{
+            input = Files.readAllLines(Path.of("task-01/input.txt"));
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return input;
+    }
+
+    public static int firstTask(){
+
+        int res = 0;
+        int dial = 50;
+
+        for(String s: getInput("task-01/input.txt")){
+            char dir = s.charAt(0);
+            int count = Integer.parseInt(s.substring(1, s.length()));
+            if(dir=='R'){
+                dial = (dial + count) % 100;
+            }else{
+                dial = (dial - count + 100) % 100;
+            }
+            if(dial==0){
+                res++;
+            }
+        }
+
+        return res;
+
+    }
+
+}
